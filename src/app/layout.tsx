5e730ebcd564200426a77bee237615ae1b70310a
emmabaka <bakaemma88@gmail.com>
2023-08-12 19:16:59 +0300

    refactor: update some styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>{children}</main>
+        <main style={{ paddingTop: "70px" }}>{children}</main>
         <Footer />
       </body>
     </html>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import s from "./Header.module.scss";
 const Header = () => {
   return (
     <header className={s.header}>
-      <div className="container">
+      <div className={`container ${s.headerContainer}`}>
         <Navigation />
       </div>
     </header>
