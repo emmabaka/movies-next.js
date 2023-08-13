d2f2673cc83d06c05f811e29f72a21cc641a00b7
emmabaka <bakaemma88@gmail.com>
2023-08-13 15:57:44 +0300

    fix: adaptive cast images size

--- src/app/movies/[movieId]/components/Cast/Cast.tsx
+++ src/app/movies/[movieId]/components/Cast/Cast.tsx
@@ -9,6 +9,7 @@ import "swiper/css";
 import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 import s from "./Cast.module.scss";
+import { useMediaQuery } from "react-responsive";
 
 const IMAGES_BASE_URL = "https://image.tmdb.org/t/p/w200/";
 
@@ -31,6 +32,27 @@ const Cast = () => {
   const [cast, setCast] = useState([]);
   const { movieId } = useParams();
 
+  const isMobile = useMediaQuery({ query: "(max-width: 550px)" });
+  const isTablet = useMediaQuery({ query: "(max-width: 860px)" });
+  const isLaptop = useMediaQuery({ query: "(max-width: 1200px)" });
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 1200px)",
+  });
+
+  const currImgValues = () => {
+    if (isMobile) {
+      return { width: 80, height: 120, quantity: 3 };
+    } else if (isTablet) {
+      return { width: 80, height: 120 };
+    } else if (isLaptop) {
+      return { width: 140, height: 200 };
+    } else if (isDesktop) {
+      return { width: 200, height: 300 };
+    } else {
+      return { width: 140, height: 200 };
+    }
+  };
+
   useEffect(() => {
     fetchMovieDetails("cast", movieId)
       .then((res) => setCast(res.cast))
@@ -44,7 +66,7 @@ const Cast = () => {
           className={s.swiper}
           modules={[Scrollbar, Autoplay]}
           spaceBetween={20}
-          slidesPerView={5}
+          slidesPerView={currImgValues()?.quantity || 5}
           scrollbar={{ draggable: true }}
           autoplay={{
             delay: 2500,
@@ -62,8 +84,8 @@ const Cast = () => {
                         : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
                     }
                     alt={item.name}
-                    width={140}
-                    height={200}
+                    width={currImgValues()?.width}
+                    height={currImgValues()?.height}
                   />
                   <p className={s.name}>{item.name}</p>
                   <p className={s.role}>Role: {item.character}</p>
